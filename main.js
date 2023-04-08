184d4796298efc1dd60064eec401cf68b75fc17c
Karine <karinemsoliveira@gmail.com>
2023-04-08 15:05:05 -0300

    Starting to build the menu

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+menuBurguer = document.querySelector('.menuBurguer')
+menuList = document.querySelector('.openedmenu')
+
+
+menuBurguer.addEventListener('click', (e)=>{
+    if(menuList.style.display = 'flex'){
+        menuList.style.display = 'none'
+       }
+   menuList.style.display = 'flex'
+   
+})
