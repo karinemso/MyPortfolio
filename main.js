ed45496e276d5bad482a68c78007d0005c86ca55
Karine <karinemsoliveira@gmail.com>
2023-04-08 16:20:05 -0300

    Navigation menu

--- main.js
+++ main.js
@@ -2,8 +2,35 @@
 
 menuBurguer = document.querySelector('.menuBurguer')
 menuList = document.querySelector('.openedmenu')
-
-
 menuBurguer.addEventListener('click', ()=>{
+    
+    menuBurguer.classList.toggle('active')
     menuList.classList.toggle('flex')
+
+    
 })
+
+navHome = document.querySelector("#NavHome")
+navSobre = document.querySelector("#NavSobre")
+
+navs = [navHome, navSobre]
+
+navs.map((nav)=>{
+    nav.addEventListener('click', ()=>{
+        menuBurguer.classList.toggle('active')
+        menuList.classList.toggle('flex')
+    
+})})
+// navHome.addEventListener('click', ()=>{
+//     menuBurguer.classList.toggle('active')
+//     menuList.classList.toggle('flex')
+
+
+// })
+
+// navSobre.addEventListener('click', ()=>{
+//     menuBurguer.classList.toggle('active')
+//     menuList.classList.toggle('flex')
+
+
+// })
